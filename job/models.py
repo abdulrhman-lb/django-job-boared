84ac93b560f947787734b882b70cf4c9f9dc4e0b
abdulrhman-lb <abdulrhman.lb2@gmail.com>
2020-06-03 22:35:56 +0300

    add job views & url

--- job/models.py
+++ job/models.py
@@ -14,6 +14,7 @@ class job(models.Model):
     vacanvy = models.IntegerField(("Vacanvy") , default=1)
     salary = models.IntegerField(("Salary") , default=0)
     experience = models.IntegerField(("Experience") , default=1)
+    category = models.ForeignKey("category", verbose_name=(""), on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("job")
@@ -24,3 +25,18 @@ class job(models.Model):
 
     def get_absolute_url(self):
         return reverse("job_detail", kwargs={"pk": self.pk})
+
+
+class category(models.Model):
+    name = models.CharField((""), max_length=25)    
+
+    class Meta:
+        verbose_name = ("category")
+        verbose_name_plural = ("categorys")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category_detail", kwargs={"pk": self.pk})
+
